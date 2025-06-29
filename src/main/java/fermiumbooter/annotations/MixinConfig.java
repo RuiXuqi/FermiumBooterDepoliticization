d7e61eb0208e63ad91735575458fa9cf5394879b
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-06-29 19:03:44 +0800

    make MixinConfig#name() has default value, although it would not break campat with version of 1.2

--- src/main/java/fermiumbooter/annotations/MixinConfig.java
+++ src/main/java/fermiumbooter/annotations/MixinConfig.java
@@ -10,7 +10,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 public @interface MixinConfig {
 
-	String name();
+	String name() default "";
 
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.FIELD)
