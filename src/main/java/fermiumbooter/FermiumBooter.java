dd161e7d770e031785cdd6cd61aca4b6b7ef5ffb
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-07-06 16:33:25 +0800

    Update FermiumBooter.java

--- src/main/java/fermiumbooter/FermiumBooter.java
+++ src/main/java/fermiumbooter/FermiumBooter.java
@@ -48,18 +48,6 @@ public class FermiumBooter
   }
 
   @Override
-  public Set<ArtifactVersion>
-  getRequirements() {
-      try {
-    return Collections.singleton(
-        new DefaultArtifactVersion("mixinbooter", VersionRange.createFromVersionSpec("[10,)")));
-      } catch (Throwable t) {
-          return Collections.singleton(
-        new DefaultArtifactVersion("mixinbooter", true));
-      }
-  }
-
-  @Override
   public List<String> getMixinConfigs() {
     return Arrays.asList(
         FermiumRegistryAPI.getLateMixins().keySet().toArray(new String[0]));
