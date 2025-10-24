034a2d3f123ea3f47349113c305eb2c174ff163a
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-10-24 22:39:52 +0800

    Attenuate

--- src/main/java/fermiumbooter/FermiumPlugin.java
+++ src/main/java/fermiumbooter/FermiumPlugin.java
@@ -1,35 +1,28 @@
 package fermiumbooter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import fermiumbooter.util.FermiumJarScanner;
 import fermiumbooter.util.ForcedConfigHandler;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.launch.MixinBootstrap;
-import org.spongepowered.asm.mixin.Mixins;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
 
 @IFMLLoadingPlugin.Name("FermiumBooter")
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.SortingIndex(990)
-public class FermiumPlugin implements IFMLLoadingPlugin {
+public class FermiumPlugin implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
 	public static final Logger LOGGER = LogManager.getLogger("FermiumBooter");
 	
 	static {
 		//Handle forced mixins reasonably early to catch crashes
 		ForcedConfigHandler.handleForcedMixinConfigs();
-	}
-
-	public FermiumPlugin() {
-		MixinBootstrap.init();
-		MixinExtrasBootstrap.init();
-		Mixins.addConfiguration("mixins.fermiumbooter.init.json");
 	}
 
 	@Override
@@ -50,15 +43,20 @@ public class FermiumPlugin implements IFMLLoadingPlugin {
 		return null;
 	}
 
-	/**
+    @Override
+    public void injectData(Map<String, Object> map) {
+    }
+
+    /**
 	 * Handle actually parsing and adding the early configurations here, as it gets called after all other plugins are initialized
 	 */
 	@Override
-	public void injectData(Map<String, Object> data) {
+    public List<String> getMixinConfigs() {
 		//Handle caching now if it hasn't already
 		FermiumJarScanner.handleCaching();
 		//Clear larger cached jar scanner fields cause why not
 		FermiumJarScanner.clearCaches();
+        List<String> mixin = new ArrayList<>();
 		
 		for(Map.Entry<String, List<Supplier<Boolean>>> entry : FermiumRegistryAPI.getEarlyMixins().entrySet()) {
 			//Check for removals
@@ -78,9 +76,10 @@ public class FermiumPlugin implements IFMLLoadingPlugin {
 			//Add configuration
 			if(enabled) {
 				LOGGER.log(Level.INFO, "FermiumBooter adding \"{}\" for early mixin application.", entry.getKey());
-				Mixins.addConfiguration(entry.getKey());
+				mixin.add(entry.getKey());
 			}
 		}
+        return mixin;
 	}
 	
 	@Override
